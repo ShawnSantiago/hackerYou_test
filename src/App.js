0fd421603158fb32af216708f033797da8abf45f
Shawn <santiagoshawn@gmail.com>
2018-05-29 20:51:10 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      items: [],
-      stores:[]
+      items: []
     };
   }
 
@@ -24,7 +23,7 @@ class App extends Component {
       .then(res => res.json())
       .then(
         (result) => {
-          console.log(result.result)
+          // console.log(result.result)
           this.setState({
             isLoaded: true,
             items: result.result
@@ -54,37 +53,60 @@ class App extends Component {
         element.classList.add('hide')
       } 
       else if (element.classList.contains('hide') ){
-         element.classList.remove('hide');
-         element.classList.add('show')
+        element.classList.remove('hide');
+        element.classList.add('show')
+        fetch("http://lcboapi.com/stores?product_id="+item_id, {
+           method: 'GET', 
+           mode: 'cors',
+           headers: new Headers({
+           Authorization : "Token MDpkZWJiMjFhYS01ZWUyLTExZTgtODMzMC1jMzU2N2UzNmI4Yjc6cWtCdUxVcnRBVzJwYTNGckZ3YUJmUjN3Y3NHV3F6eDJGdkJU"
+          })
+        })
+        .then(res => res.json())
+        .then(
+          (result) => {
+            const stores_item = result.result
+            const in_stores = stores_item.map(function(item){
+              const element = document.getElementById(item_id).childNodes[2].childNodes[1]
+              // element.append(<li key={item.name}>{item.name}</li>)
+              element.append("<li key="+{item.name}+">"+{item.name}+"</li>")
+            })
+           
+            console.dir(element)
+            // return in_stores
+          }
+        )
       }
       else {
-         element.classList.add('show')
-      }
-    }
-    const in_store = (item) => {
-   
-      console.log(item)
-      fetch("http://lcboapi.com/stores?product_id="+item, {
-             method: 'GET', 
-             mode: 'cors',
-             headers: new Headers({
-             Authorization : "Token MDpkZWJiMjFhYS01ZWUyLTExZTgtODMzMC1jMzU2N2UzNmI4Yjc6cWtCdUxVcnRBVzJwYTNGckZ3YUJmUjN3Y3NHV3F6eDJGdkJU"
+        element.classList.add('show')
+        fetch("http://lcboapi.com/stores?product_id="+item_id, {
+           method: 'GET', 
+           mode: 'cors',
+           headers: new Headers({
+           Authorization : "Token MDpkZWJiMjFhYS01ZWUyLTExZTgtODMzMC1jMzU2N2UzNmI4Yjc6cWtCdUxVcnRBVzJwYTNGckZ3YUJmUjN3Y3NHV3F6eDJGdkJU"
+          })
         })
-      })
-      .then(res => res.json())
-      .then(
-        (result) => {
-           const stores_item = result.result
-            const stores = stores_item.map(function(item){
-              return <li key={item.name}>{item.name}</li>
+        .then(res => res.json())
+        .then(
+          (result) => {
+            const stores_item = result.result
+            const in_stores = stores_item.map(function(item){
+               const element = document.getElementById(item_id).childNodes[2].childNodes[1]
+              element.append("<li key="+{item.name}+">"+{item.name}+"</li>")
+              // console.log(element.append(<li key={item.name}>{item.name}</li>))
             })
-             
-             
-        }
-      )
-     
+      
+           // return in_stores
+          }
+        )
+      }
+
     }
+
+    
     const li = function (item) { 
+      // console.log(in_store);
+      
       return <li key={item.name} id={item.id} onClick={(e) => handleClick(item, e)}>
               <div className="card_header">
                 <h1>{item.name} </h1>
@@ -95,7 +117,8 @@ class App extends Component {
               <div className="card_desc"> 
                 <p> {item.tags} </p>
 
-                <ul>{in_store(item.id)} </ul>
+                <ul>
+                </ul>
               </div>
             </li>
           };
@@ -109,9 +132,11 @@ class App extends Component {
         if (item.is_seasonal) {
           if (!item.image_thumb_url) {
             item.image_thumb_url = "https://www.crafthounds.com/wp-content/uploads/2016/11/No-Image-Available.png"
+           
             return ( li(item) );
             
           }else {
+            
              return ( li(item) );
           }
         }
